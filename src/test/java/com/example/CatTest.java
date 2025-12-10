c385f360fb868b6e32bededcfa762bb439d80a89
Dina <dinapotanay@yandex.ru>
2025-12-10 23:14:16 +0300

    Commit

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 class CatTest {
-  
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundReturnCatSound() {
+        Cat cat = new Cat(feline);
+        String expectedResult = "Мяу";
+        String actualResult = cat.getSound();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void getFoodReturnCatFood() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expectedResult = Arrays.asList("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expectedResult);
+        List<String> actualResult = cat.getFood();
+        assertEquals(expectedResult, actualResult);
+    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,4 +1,44 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class LionTest {
-  
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensReturnLionKittens() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        int expectedResult = 5;
+        Mockito.when(feline.getKittens()).thenReturn(expectedResult);
+        int actualResult = lion.getKittens();
+        assertEquals(expectedResult, actualResult, "Количество детенышей не соответствует ожидаемому");
+    }
+
+    @Test
+    public void getFoodReturnLionFood() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        List<String> expectedResult = Arrays.asList("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(expectedResult);
+        List<String> actualResult = lion.getFood();
+        assertEquals(expectedResult, actualResult, "Тип еды не соответствует ожидаемой для львов");
+    }
+
+    @Test
+    public void constructorIncorrectSex() {
+        assertThrows(Exception.class, () -> new Lion("Женщина", feline));
+    }
+
 }
