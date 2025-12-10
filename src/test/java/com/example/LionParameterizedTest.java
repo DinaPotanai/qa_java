51afa2470f95acf8819a891155504caab8b3cc1a
Dina <dinapotanay@yandex.ru>
2025-12-10 22:08:32 +0300

    first commit

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class LionTest {
+
+    @Mock
+    private Feline feline;
+
+    @ParameterizedTest
+    @CsvSource({
+            "Самец, true",
+            "Самка, false"
+    })
+    void testDoesHaveMane(String sex, boolean expectedResult) throws Exception {
+        Lion lion = new Lion(sex, feline);
+        boolean actualResult = lion.doesHaveMane();
+        assertEquals(expectedResult, actualResult, "Используйте допустимые значения пола животного");
+    }
+
+
+    }
